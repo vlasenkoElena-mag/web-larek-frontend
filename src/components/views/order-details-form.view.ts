de4639f517f0e3fee30d310d63837c8615ea21e1
ElenaVlasenko <radionovaelena124@gmail.com>
2026-04-17 01:45:23 +0300

    delete unnesassary properties

--- src/components/views/order-details-form.view.ts
+++ src/components/views/order-details-form.view.ts
@@ -1,3 +1,4 @@
+import type { OrderDetails } from '../../types';
 import type { OrderDetailsFormView, OrderDetailsViewEvents } from '../../types/views/order-details.view';
 import { ensureElement } from '../../utils/utils';
 import { ObservableObject } from '../base/observable-object';
@@ -6,10 +7,9 @@ type Params = {
     form: HTMLFormElement;
 };
 
+/** Представление формы деталей заказа. */
 export class OrderDetailsFormBrowserView extends ObservableObject<OrderDetailsViewEvents> implements OrderDetailsFormView {
     private _form: HTMLFormElement;
-    private _address = '';
-    private _paymentMethod: 'card' | 'cash' | '' = '';
     private _orderButton: HTMLButtonElement;
     private _addressInput: HTMLInputElement;
     private _paymentMethodCardEl: HTMLButtonElement;
@@ -26,12 +26,41 @@ export class OrderDetailsFormBrowserView extends ObservableObject<OrderDetailsVi
         this.addHandlers();
     }
 
+    public render({ address, payment }: OrderDetails): void {
+        this._addressInput.value = address;
+        if (payment === 'card') {
+            this._paymentMethodCardEl.classList.add('button_alt-active');
+            this._paymentMethodCashEl.classList.remove('button_alt-active');
+        }
+        else if (payment === 'cash') {
+            this._paymentMethodCashEl.classList.add('button_alt-active');
+            this._paymentMethodCardEl.classList.remove('button_alt-active');
+        }
+        else {
+            this._paymentMethodCardEl.classList.remove('button_alt-active');
+            this._paymentMethodCashEl.classList.remove('button_alt-active');
+        }
+    }
+
+    enableSubmitButton(): void {
+        this._orderButton.disabled = false;
+    }
+
+    disableSubmitButton(): void {
+        this._orderButton.disabled = true;
+    }
+
     addHandlers() {
+        const getPayment = (): 'card' | 'cash' | '' => {
+            if (this._paymentMethodCardEl.classList.contains('button_alt-active')) return 'card';
+            if (this._paymentMethodCashEl.classList.contains('button_alt-active')) return 'cash';
+            return '';
+        };
+
         const onAddressInput = () => {
-            this.setOrderButtonDisabledState(this._validate());
+            const address = this._addressInput.value.trim();
             this._emit('FORM-CHANGED', {
-                data: { address: this._address, payment: this._paymentMethod },
-                isValid: this._validate(),
+                data: { address, payment: getPayment() },
             });
         };
         this._addressInput.addEventListener('input', onAddressInput);
@@ -39,10 +68,8 @@ export class OrderDetailsFormBrowserView extends ObservableObject<OrderDetailsVi
         const onPaymentCardClick = () => {
             this._paymentMethodCardEl.classList.add('button_alt-active');
             this._paymentMethodCashEl.classList.remove('button_alt-active');
-            this._paymentMethod = 'card';
             this._emit('FORM-CHANGED', {
-                data: { address: this._address, payment: this._paymentMethod },
-                isValid: this._validate(),
+                data: { address: this._addressInput.value.trim(), payment: 'card' },
             });
         };
         this._paymentMethodCardEl.addEventListener('click', onPaymentCardClick);
@@ -50,29 +77,21 @@ export class OrderDetailsFormBrowserView extends ObservableObject<OrderDetailsVi
         const onPaymentCashClick = () => {
             this._paymentMethodCashEl.classList.add('button_alt-active');
             this._paymentMethodCardEl.classList.remove('button_alt-active');
-            this._paymentMethod = 'cash';
             this._emit('FORM-CHANGED', {
-                data: { address: this._address, payment: this._paymentMethod },
-                isValid: this._validate(),
+                data: { address: this._addressInput.value.trim(), payment: 'cash' },
             });
         };
         this._paymentMethodCashEl.addEventListener('click', onPaymentCashClick);
 
         this._onSubmit = (evt: Event) => {
             evt.preventDefault();
+            const address = this._addressInput.value.trim();
+            const payment = getPayment();
             this._emit('FORM-SUBMIT', {
-                address: this._address,
-                payment: this._paymentMethod,
+                address,
+                payment,
             });
         };
         this._form.addEventListener('submit', this._onSubmit);
-    }
-
-    private _validate(): boolean {
-        return this._addressInput.value.trim() !== '' && this._paymentMethod !== '';
-    }
-
-    setOrderButtonDisabledState(disabled: boolean): void {
-        this._orderButton.disabled = disabled;
     }
 }
