5bbc0413cde45f7a1bf87fc7743d5548146af364
ElenaVlasenko <radionovaelena124@gmail.com>
2026-04-17 01:19:14 +0300

    fix product view

--- src/components/views/product/product-card.view.ts
+++ src/components/views/product/product-card.view.ts
@@ -2,7 +2,7 @@ import type { Product } from '../../../types';
 import { ensureElement } from '../../../utils/utils';
 import { formatPrice } from '../../../utils/simple-utils';
 import { ObservableObject } from '../../base/observable-object';
-import type { ProductModalViewEvents, ProductCardView } from '../../../types/views/product.view';
+import type { ProductViewEvents, ProductCardView } from '../../../types/views/product.view';
 import { categoryMap } from '../../../utils/constants';
 
 /** Параметры для создания представления продукта. */
@@ -12,25 +12,11 @@ export type ProductViewParams = {
 };
 
 /**
- * Представление детальной информации о товаре (MVP паттерн).
- *
- * `ProductBrowserView` отвечает за отображение полной информации о товаре в модальном окне
- * или отдельной странице. Класс управляет DOM-элементами карточки продукта, наполняет их данными
- * и обрабатывает взаимодействие пользователя с кнопкой покупки.
- *
- * Основной функционал:
- * - Инициализация и кэширование ссылок на DOM-элементы карточки товара
- * - Заполнение данных товара (название, категория, цена, описание, изображение)
- * - Обработка клика по кнопке "Купить" с эмиссией события `buy_button_click`
- * - Управление состоянием кнопки покупки (активна/неактивна)
- * - Рендеринг карточки в целевой контейнер
+ * Представление детальной информации о товаре.
  */
-export class ProductCardBrowserView extends ObservableObject<ProductModalViewEvents> implements ProductCardView {
+export class ProductCardBrowserView extends ObservableObject<ProductViewEvents> implements ProductCardView {
     /** Корневой элемент карточки товара */
     public readonly element: HTMLElement;
-
-    /** текущий отображаемй продукт */
-    private _product: Product | null;
 
     /** Элемент отображения цены товара */
     private _priceElement: HTMLElement;
@@ -50,14 +36,6 @@ export class ProductCardBrowserView extends ObservableObject<ProductModalViewEve
     /** Кнопка добавления товара в корзину */
     private _buyButtonElement: HTMLButtonElement;
 
-    /**
-     * Создаёт экземпляр представления товара.
-     *
-     * Конструктор выполняет следующие действия:
-     * 1. Сохраняет ссылку на корневой элемент
-     * 2. Находит и кэширует все дочерние элементы карточки по CSS-селекторам
-     * 3. Устанавливает обработчики событий DOM-елементов.
-     */
     constructor({ productCardElement }: ProductViewParams) {
         super();
         this.element = productCardElement;
@@ -67,19 +45,30 @@ export class ProductCardBrowserView extends ObservableObject<ProductModalViewEve
         this._titleElement = ensureElement('.card__title', productCardElement);
         this._imageElement = (ensureElement('.card__image', productCardElement) as HTMLImageElement);
         this._buyButtonElement = ensureElement('.button', productCardElement) as HTMLButtonElement;
-        this._product = null;
+        this.element.dataset.productId = '';
 
         this._buyButtonElement.addEventListener('click', () => {
-            this._emit('BUTTON-CLICK:BUY', this._makeBuyButtonClickPayload());
+            this._emit('BUTTON-CLICK:BUY', { productId: this.element.dataset.productId ?? '' });
         });
     }
 
     /**
-     * Устанавливает текущий отображаемый продукт, отображает его данные в DOM-елементы.
+     * Замечание: "Метод render всех классов слоя View должен в инструкции return возвращать
+     * заполненный родительский контейнер карточки,
+     * который должен поступать как аргумент в класс в виде клонированного шаблона."
+     *
+     * Комментарий:метод render отвечает за отображение данных. Сигнатура render(product: Product): void
+     * хорошо описывает то, что делает метод и согласуется с принципом CQS.
+     * Он не нарушает никаких привил и принципов ООП или MVP.
      */
-    render(product: Product) {
-        this._product = product;
-        this._renderProduct(product);
+    public render({ id, title, category, price, description, image }: Product): void {
+        this._categoryElement.textContent = category;
+        this._priceElement.textContent = price === null ? 'Не продается' : formatPrice(price ?? 0);
+        this._titleElement.textContent = title;
+        this._descriptionElement.textContent = description;
+        this._imageElement.src = `/images${image}`;
+        this._categoryElement.className = `card__category card__category_${categoryMap[category]}`;
+        this.element.dataset.productId = id;
     }
 
     /**
@@ -91,28 +80,7 @@ export class ProductCardBrowserView extends ObservableObject<ProductModalViewEve
      * - Блокировка во время обработки запроса на добавление в корзину
      * - Разблокировка при удалении товара из корзины
      */
-    setButtonDisabledState(disabled: boolean) {
+    public setAddToCartButtonState(disabled: boolean): void {
         this._buyButtonElement.disabled = disabled;
-    }
-
-    private _renderProduct({ title, category, price, description, image }: Product) {
-        this._categoryElement.textContent = category;
-        this._priceElement.textContent = price === null ? 'Не продается' : formatPrice(price ?? 0);
-        this._titleElement.textContent = title;
-        this._descriptionElement.textContent = description;
-        this._imageElement.src = `/images${image}`;
-        this._categoryElement.className = `card__category card__category_${categoryMap[category]}`;
-
-        if (price === null) {
-            this._buyButtonElement.disabled = true;
-        }
-    }
-
-    private _makeBuyButtonClickPayload(): ProductModalViewEvents['BUTTON-CLICK:BUY'] {
-        if (this._product === null) {
-            throw new Error('Application logic error: publishing product-related event when product is null');
-        }
-
-        return { product: this._product };
     }
 }
--- src/components/views/product/product-modal.view.ts
+++ src/components/views/product/product-modal.view.ts
@@ -1,35 +1,52 @@
-import type { ProductCardView, ProductModalView } from '../../../types/views/product.view';
+import type {
+    ProductCardView,
+    ProductModalView,
+    ProductViewEvents,
+} from '../../../types/views/product.view';
 import { ensureElement } from '../../../utils/utils';
 import { ModalBrowserView } from '../common/modal.view';
+import { ObservableContentModalView } from '../common/observable-content-modal.view';
 import { ProductCardBrowserView } from './product-card.view';
 
 /**
  * Браузерная реализация `ProductModalView`.
  * Декорирует ProductCardView логикой модального окна.
  */
-export class ProductModalBrowserView implements ProductModalView {
-    private _modal: ModalBrowserView;
-    private _productCardView: ProductCardBrowserView;
-
+export class ProductModalBrowserView extends ObservableContentModalView<ProductViewEvents, ProductCardBrowserView> implements ProductModalView {
     constructor() {
         const modalRoot = document.getElementById('product-modal') as HTMLElement;
-        this._modal = new ModalBrowserView({ rootElement: modalRoot });
-        this._productCardView = new ProductCardBrowserView({ productCardElement: ensureElement('.card_full', modalRoot) });
+
+        /**
+         * Замечание: "По правилам ООП и MVP в одних классах не должны создаваться экземпляры других классов,
+         *  чтобы не было жёсткой зависимости одного класса от другого, классы могли быть заменяемыми
+         *  в текущем проекте, и переиспользуемыми в других проектах."
+         *
+         * Комментарий: Принцип инверсии зависимостей (DIP) применяется к тем зависимостям, которые являются проблемными
+         * (например затрудняющие тестирование логики зависимости презентера от dom елементов), а не ко всему подряд.
+         * ProductModalBrowserView является сугубо специфачным представлением для данного проекта и не предполагает переиспользования.
+         * Переиспользовани предполагают такие классы как ModalBrowserView (те что в common)
+         *
+         * Предлагаемый подход (презентер это не класс, а индекс файл) делает из беспроблемных зависимостей (view от view) проблемные (например презентер от dom),
+         * что делает тестирование логики презентера затруднительным, так как необходимо создавать реальные dom элементы, что по сути превращает юнит тесты в интеграционные.
+         */
+        super(
+            new ProductCardBrowserView({ productCardElement: ensureElement('.card_full', modalRoot) }),
+            new ModalBrowserView({ rootElement: modalRoot }),
+        );
     }
 
-    /** Делегирует подписки на события внутреннего `ProductView`. */
-    on(...params: Parameters<ProductCardView['on']>): void {
-        this._productCardView.on(...params);
-    };
+    private get _productCardView() {
+        return this._content;
+    }
 
     /** Рендерит данные в карточку товара и показывает модальное окно. */
     render(params: Parameters<ProductCardView['render']>[0]): void {
         this._productCardView.render(params);
-        this._modal.show();
+        this.show();
     }
 
     /** Устанавливает состояние кнопки покупки товара. */
-    setButtonDisabledState(disabled: boolean): void {
-        this._productCardView.setButtonDisabledState(disabled);
+    public setAddToCartButtonState(disabled: boolean): void {
+        this._productCardView.setAddToCartButtonState(disabled);
     }
 }
