7b193da10191138ff4f7bcded42908eab4ecd9ba
ElenaVlasenko <radionovaelena124@gmail.com>
2026-04-17 01:43:36 +0300

    fix OrderCreationResultModalBrowserView

--- src/components/views/order-creation-result-modal.view.ts
+++ src/components/views/order-creation-result-modal.view.ts
@@ -1,28 +1,32 @@
 import type { OrderItems } from '../../types';
-import type { OrderCreationResultModalView } from '../../types/views/order-creation-result.view';
+import type { OrderCreationResultModalView, OrderCreationResultView, OrderCreationResultViewEvents } from '../../types/views/order-creation-result.view';
 import { ModalBrowserView } from './common/modal.view';
+import { ObservableContentModalView } from './common/observable-content-modal.view';
 import { OrderCreationResultBrowserView } from './order-creation-result.view';
 
-export class OrderCreationResultModalBrowserView implements OrderCreationResultModalView {
-    private _modal: ModalBrowserView;
-    private _contentOfSuccessModalRoot: OrderCreationResultBrowserView;
-
+/**
+ * Браузерная реализация `OrderCreationResultModalView`.
+ * Декорирует OrderCreationResultView логикой модального окна.
+ */
+export class OrderCreationResultModalBrowserView
+    extends ObservableContentModalView<OrderCreationResultViewEvents, OrderCreationResultView> implements OrderCreationResultModalView {
     constructor() {
         const modalRoot = document.getElementById('success-modal') as HTMLElement;
-        const contentOfSuccessModalRoot = modalRoot.querySelector('.order-success') as HTMLElement;
-        this._modal = new ModalBrowserView({ rootElement: modalRoot });
-        this._contentOfSuccessModalRoot = new OrderCreationResultBrowserView({ contentOfSuccessModalRoot });
-        this._contentOfSuccessModalRoot.on('ORDER-CREATION-RESULT:CLOSED', () => {
-            this._modal.hide();
-        });
-    }
 
-    on(...params: Parameters<OrderCreationResultBrowserView['on']>): void {
-        this._contentOfSuccessModalRoot.on(...params);
+        const content = new OrderCreationResultBrowserView({
+            contentOfSuccessModalRoot: modalRoot.querySelector('.order-success') as HTMLElement,
+        });
+
+        super(
+            content,
+            new ModalBrowserView({ rootElement: modalRoot }),
+        );
+
+        content.on('ORDER-CREATION-RESULT:CLOSED', () => this.hide());
     }
 
     render(totalPrice: OrderItems['total']): void {
-        this._contentOfSuccessModalRoot.render(totalPrice);
-        this._modal.show();
+        this._content.render(totalPrice);
+        this.show();
     }
 }
